a6021e275d3c3cc21bcbec7a97eb991968f65390
universgroup <univers.tech.group@gmail.com>
2026-06-24 18:40:35 +0000

    Personnalisation réussie pour la gestion des classes

--- stsavenirafrique/settings.py
+++ stsavenirafrique/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # MEDIA_DIR = BASE_DIR/'media'
 
