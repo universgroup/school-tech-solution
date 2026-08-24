d6903d302b7787958c8f59206f1407ffff72d484
universgroup <univers.tech.group@gmail.com>
2026-08-24 06:24:51 +0000

    Modification de la valeur de Debug

--- stsavenirafrique/settings.py
+++ stsavenirafrique/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # Application definition
 
