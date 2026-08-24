a02bf3620ea6cd945f2a2d8ac7695aa8aec1248f
universgroup <univers.tech.group@gmail.com>
2026-08-24 14:31:01 +0000

    Installation et configuration de whitenoise dans les settings et wsgi du projet

--- stsavenirafrique/settings.py
+++ stsavenirafrique/settings.py
@@ -148,6 +148,8 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # MEDIA_DIR = BASE_DIR/'media'
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
